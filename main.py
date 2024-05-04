f1a9e833f0643b1ccf04d333684cafd75fdef46f
lltl <a@a.com>
2024-05-04 10:01:01 +0800

    第3周
    Signed-off-by: lltl <a@a.com>

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
 import os
 
 if __name__ == "__main__":
-    path = "datasets\\apple_banana_datasets\\train"
-    labels = [f.name for f in os.scandir(path) if f.is_dir()]
-    imgs = []
-    # test
-    for label in labels:
-        # 获取这个标签下面的所有文件
-        for img_file in os.listdir(os.path.join(path, label)):
-            imgs.append((os.path.join(path, label, img_file), label))
-    print(imgs)
+    print("test")
